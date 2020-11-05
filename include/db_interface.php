67d04420247a923b1f6bab8b669db8a56357ed22
Patrick Thomas <pwt5ca@virginia.edu>
2020-11-05 15:12:38 -0500

    Simple example to display top 5 recent movies

--- include/db_interface.php
+++ include/db_interface.php
@@ -7,49 +7,52 @@ session_start();
 /**
  * Create a new user given an email and a password.
  */
-function create_new_user($email, $password) {
+function create_new_user($email, $password)
+{
     global $db;
 
     $password_hashed = password_hash($password, PASSWORD_DEFAULT);
-    $sql = "INSERT INTO Users (email, password) VALUES (:email, :password)";
+    $sql = "INSERT INTO Users (email, password) VALUES (?, ?)";
 
     $statement = $db->prepare($sql);
-    $statement->execute(array(
-        ":email" => $email,
-        ":password" => $password_hashed,
-    ));
-    $statement->closeCursor();
+    $statement->bind_param("ss", $name, $password_hashed);
+    if (!$statement->execute()) {
+        die("Error executing create_new_user query.");
+    }
+    $statement->close();
 }
 
 /**
  * Log into a user and set a session variable indicating what user is logged in currently.
  */
-function login_user($email, $password) {
+function login_user($email, $password)
+{
     global $db;
 
-    $sql = "SELECT password FROM Users WHERE email=:email";
+    $sql = "SELECT password FROM Users WHERE email=?";
 
     $statement = $db->prepare($sql);
-    $statement->execute(array(
-        ":email" => $email,
-    ));
+    $statement->bind_param("s", $email);
     $results = $statement->fetch();
+    $stored_password = "";
+    $statement->bind_result($stored_password);
     if ($results) {
-        if (password_verify($password, $results['password'])) {
+        if (password_verify($password, $stored_password)) {
             $_SESSION['email'] = $email;
             $_SESSION['password'] = $password; // This is a bad practice.
-            $statement->closeCursor();
+            $statement->close();
             return true;
         }
     }
-    $statement->closeCursor();
+    $statement->close();
     return false;
 }
 
 /**
  * Return true if the current user is logged in, otherwise return false.
  */
-function is_logged_in() {
+function is_logged_in()
+{
     if (!isset($_SESSION['email']) || !isset($_SESSION['password'])) {
         return false;
     }
@@ -57,4 +60,37 @@ function is_logged_in() {
     $password = $_SESSION['password']; // This is a bad practice.
 
     return login_user($email, $password);
+}
+
+/**
+ * Return true if the current user is logged in, otherwise return false.
+ * 
+ * From https://www.php.net/manual/en/mysqli-stmt.fetch.php
+ */
+function get_top_five_movies()
+{
+    global $db;
+    $sql = "SELECT primaryTitle FROM pwt5ca.Titles ORDER BY startYear DESC LIMIT 5";
+    $output = array();
+
+    if ($stmt = $db->prepare($sql)) {
+
+        /* execute statement */
+        $stmt->execute();
+    
+        /* bind result variables */
+        $title = "";
+        $output = array();
+        $stmt->bind_result($title);
+    
+        /* fetch values */
+        while ($stmt->fetch()) {
+            array_push($output, $title);
+        }
+    
+        /* close statement */
+        $stmt->close();
+    }
+
+    return $output;
 }
--- index.php
+++ index.php
@@ -1,3 +1,21 @@
 <?php
 
 require_once("include/db_interface.php");
+
+?>
+
+
+<html>
+
+<head>
+</head>
+
+<body>
+    <?php
+        $top_five = get_top_five_movies();
+
+        echo json_encode($top_five);
+    ?>
+</body>
+
+</html>
