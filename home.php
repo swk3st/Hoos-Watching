9d9bf106053def0bdee6c9ee075a97280c24356e
Jessica Heavner <jlh9qv@virginia.edu>
2020-11-12 17:54:45 -0500

    added search bar

--- home.php
+++ home.php
@@ -18,6 +18,25 @@ if (is_null($title)) {
     die();
 }
 
+<table
+  id="table"
+  data-url="json/data1.json"
+  data-filter-control="true"
+  data-show-search-clear-button="true">
+  <thead>
+    <tr>
+      <th data-field="id">ID</th>
+      <th data-field="name" data-filter-control="input">Item Name</th>
+      <th data-field="price" data-filter-control="select">Item Price</th>
+    </tr>
+  </thead>
+</table>
+
+<script>
+  $(function() {
+    $('#table').bootstrapTable()
+  })
+</script>
 
 $HEADER_INFO = array(
     "Hoo's Watching | " . $title['primaryTitle'],
