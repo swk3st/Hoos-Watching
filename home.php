2e37ad906a339c8d87bb613c0bc2b0b1eb162a67
Jessica Heavner <jlh9qv@virginia.edu>
2020-11-12 18:06:33 -0500

    fixed search

--- home.php
+++ home.php
@@ -1,3 +1,4 @@
+<html>
 <?php
 
 require_once("include/db_interface.php");
@@ -18,25 +19,26 @@ if (is_null($title)) {
     die();
 }
 
-<table
-  id="table"
-  data-url="json/data1.json"
-  data-filter-control="true"
-  data-show-search-clear-button="true">
-  <thead>
-    <tr>
-      <th data-field="id">ID</th>
-      <th data-field="name" data-filter-control="input">Item Name</th>
-      <th data-field="price" data-filter-control="select">Item Price</th>
-    </tr>
-  </thead>
-</table>
+    <table
+    id="table"
+    data-url="json/data1.json"
+    data-filter-control="true"
+    data-show-search-clear-button="true">
+    <thead>
+        <tr>
+        <th data-field="id">ID</th>
+        <th data-field="name" data-filter-control="input">Item Name</th>
+        <th data-field="price" data-filter-control="select">Item Price</th>
+        </tr>
+    </thead>
+    </table>
 
-<script>
-  $(function() {
-    $('#table').bootstrapTable()
-  })
-</script>
+    <script>
+    $(function() {
+        $('#table').bootstrapTable()
+    })
+    </script>
+
 
 $HEADER_INFO = array(
     "Hoo's Watching | " . $title['primaryTitle'],
@@ -45,3 +47,4 @@ $HEADER_INFO = array(
 );
 include("include/boilerplate/head.php");
 ?>
+</html>
