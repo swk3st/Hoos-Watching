2717b4235c6ee5a8b510d688a9537be2bc0668f2
JCorn64 <juliancorn64@gmail.com>
2020-11-12 17:12:34 -0500

    Combined login and frontend branches

--- sign_up.php
+++ sign_up.php
@@ -3,34 +3,36 @@
 
 <head>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <link rel="stylesheet" href="signin.css">
+    <link rel="stylesheet" href="assets/css/signin.css">
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 
 </head>
 
 <body>
     <div id="login">
-        <h3 class="text-center text-white pt-5">Hoo's Watching Login</h3>
+        <h3 class="text-center text-white pt-5">Register</h3>
         <div class="container">
             <div id="login-row" class="row justify-content-center align-items-center">
                 <div id="login-column" class="col-md-6">
                     <div id="login-box" class="col-md-12">
                         <form id="login-form" class="form" action="" method="post">
-                            <h3 class="text-center text-info">Login</h3>
+                            <h3 class="text-center text-info">Register</h3>
                             <div class="form-group">
                                 <label for="username" class="text-info">Email:</label><br>
                                 <input type="text" name="email" id="email" class="form-control">
                             </div>
                             <div class="form-group">
                                 <label for="password" class="text-info">Password:</label><br>
-                                <input type="text" name="password" id="password" class="form-control">
+                                <input type="text" name="passowrd" id="passowrd" class="form-control">
                             </div>
                             <div class="form-group">
-                                <label for="remember-me" class="text-info"><span>Remember me</span> <span><input id="remember-me" name="remember-me" type="checkbox"></span></label><br>
+                                <label for="password" class="text-info">Confirm Password:</label><br>
+                                <input type="text" name="conf_pass" id="conf_pass" class="form-control">
+                                </br>
                                 <input type="submit" name="submit" class="btn btn-info btn-md" value="submit">
                             </div>
-                            <div id="register-link" class="text-right">
-                                <a href="sign_up.html" class="text-info">Register here</a>
+                            <div id="login-link" class="text-right">
+                                <a href="sign_in.html" class="text-info">Login here</a>
                             </div>
                         </form>
                     </div>
