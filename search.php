1708f48849be09cba30dfd4a1382e15b903fe03c
Jessica Heavner <jlh9qv@virginia.edu>
2020-11-13 11:06:05 -0500

    dropdown test

--- search.php
+++ search.php
@@ -11,6 +11,7 @@
             <tr>
             <th data-field="name" data-filter-control="input">Name</th>
             <th data-field="price" data-filter-control="select">Type</th>
+            <option value="movie">Movie</option>
             <th data-field="id">Year</th>
             <th data-field="id">Length</th>
             <th data-field="id">IMDb rating</th>
