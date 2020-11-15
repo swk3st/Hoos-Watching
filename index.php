ed0b0da5759651b2faba4c1257df29421188fdf9
Jessica Heavner <jlh9qv@virginia.edu>
2020-11-15 10:53:14 -0500

    test frontend

--- index.php
+++ index.php
@@ -35,9 +35,44 @@ $HEADER_INFO = array(
 );
 include("include/boilerplate/head.php");
 
-include("newsearch.php");
+// include("newsearch.php");
 
 ?>
+<html>   
+
+
+<style>
+    input-group mb-3 {
+      background-color: yellow; 
+      list-style-type: none;
+      text-align: center;
+      margin: 0;
+      padding: 0;
+    }
+    
+    
+</style>
+
+</div>
+</div>
+<div class="input-group mb-3">
+    <div class="input-group-prepend">
+    
+    <div class="search">
+        <input type="text" class="search-input" placeholder="Search for Movies, TV shows, actors, directors...">
+    
+    <select class="custom-select" id="inputGroupSelect01">
+      <option selected>Choose...</option>
+      <option value="1">Movie</option>
+      <option value="2">TV Show</option>
+      <option value="3">Actor</option>
+      <option value="3">Director</option>
+      <option value="3">Genre</option>
+    </select>
+
+  <button class="btn" type="submit"><i class="fas fa-search">Submit</i></button>
+</div>
+</html>
 
 <div class="container">
     <table class="table table-striped table-sm">
